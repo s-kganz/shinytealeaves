41aab99602710f545901a89ed81883bed8b73dfb
s-kganz <ganzkeenan1@gmail.com>
2023-03-06 16:36:53 -0800

    Define sliders, set up basic temperature output.

--- app.R
+++ app.R
@@ -1,51 +1,143 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(tealeaves)
+library(units)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
+  
+  titlePanel("Sliders"),
+  
+  # Make MathJax available
+  withMathJax(),
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+  fluidRow(
+    
+    # Define all the tealeaves parameters. One column for leaf params, one
+    # for env params.
+    
+    # Leaf parameters
+    sidebarPanel(
+      width = 2,
+      style = "height: 90vh; overflow-y: auto;",
+      
+      sliderInput("abs_l", "Emissivity (unitless)",
+                  min = 0, max = 1,
+                  value = 0.97, step=0.01),
+       
+      sliderInput("abs_s", "Shortwave absorptance (unitless)",
+                  min = 0, max = 1,
+                  value = 0.5, step=0.01),
+      
+      sliderInput(
+        "g_sw", 
+        "Stomatal conductance (umol m\\(^{-2}\\) s\\(^{-1}\\) Pa\\(^{-1}\\))",
+        min = 0, max = 100, value = 5, step=0.1
+      ),
+      
+      sliderInput(
+        "g_uw", 
+        "Cuticular conductance (umol m\\(^{-2}\\) s\\(^{-1}\\) Pa\\(^{-1}\\))",
+        min = 0, max = 10, value = 0.5, step=0.1
+      ),
+      
+      sliderInput("leafsize", 
+                  "Leaf size (cm)",
+                  min = 0, max = 50,
+                  value = 10, step=1),
+      
+    ),
+    # Env parameters
+    sidebarPanel(
+      width = 2,
+      style = "height: 90vh; overflow-y: auto;",
+      
+      sliderInput("P", 
+                  "Atmospheric pressure (kPa)",
+                  min = 0, max = 100,
+                  value = 100, step=1),
+      
+      sliderInput("r", 
+                  "Albedo (unitless)",
+                  min = 0, max = 1,
+                  value = 0.2, step=0.01),
+      
+      sliderInput("RH", 
+                  "Relative humidity (%)",
+                  min = 0, max = 100,
+                  value = 50, step=1),
+      
+      sliderInput("S_sw", 
+                  "Downwelling shortwave radiation (W m\\(^{-2}\\))",
+                  min = 0, max = 1200,
+                  value = 1000, step=10),
+      
+      sliderInput("T_air", 
+                  "Air temperature (K)",
+                  min = 275, max = 325,
+                  value = 298, step=1),
+      
+      sliderInput("wind", 
+                  "Wind speed (m s\\(^{-1}\\))",
+                  min = 0, max = 20,
+                  value = 2, step=0.2),
+      
+    ),
+    # Tabs to plot leaf T as a function of a variable or look at the detailed
+    # components of the energy balance budget.
+    column(
+      8,
+      tabsetPanel(
+        tabPanel(
+          "Plot",
+          plotOutput("plot")
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+        tabPanel(
+          "Details",
+          textOutput("leaf_t")
         )
+      )
     )
+  )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  get_t_leaf <- reactive({
+    # Collect all the parameters
+    env_parameters <- make_enviropar(
+      list(
+        P     = set_units(input$P, "kPa"),
+        r     = set_units(input$r, "1"),
+        RH    = set_units(input$RH/100, "1"),
+        S_sw  = set_units(input$S_sw, "W/m^2"),
+        T_air = set_units(input$T_air, "K"),
+        wind  = set_units(input$wind, "m/s")
+      )
+    )
+    
+    leaf_parameters <- make_leafpar(
+      replace = list(
+        abs_l    = set_units(input$abs_l, "1"),
+        abs_s    = set_units(input$abs_s, "1"),
+        g_sw     = set_units(input$g_sw, "umol/m^2/s/Pa"),
+        g_uw     = set_units(input$g_uw, "umol/m^2/s/Pa"),
+        leafsize = set_units(input$leafsize, "m")
+      )
+    )
+    
+    # And get the modeled leaf temperature
+    result <- tleaf(
+      leaf_parameters, env_parameters, make_constants(), quiet=TRUE
+    )
+    
+    # Drop units to play nice with renderText()
+    drop_units(result$T_leaf)
+    
+  })
+  
+  output$leaf_t <- renderText({
+    paste0(format(get_t_leaf(), digits=2), " K")
+  })
+  
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
